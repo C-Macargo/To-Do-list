fa60c8b6f7dba903589611ecb122c84156ce58fe
C-Macargo <caio.camargos97@gmail.com>
2023-10-04 10:27:17 -0300

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,html}',
+    './components/**/*.{js,ts,jsx,tsx,html}',
+    "./src/**/*.{js,ts,jsx,tsx,html}",
+  ],
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+  
+}
